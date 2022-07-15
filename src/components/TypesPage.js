745517cdc256733a21fec9b28941d410970a1927
hector <hectornuno1994@gmail.com>
2022-07-15 09:46:47 -0700

    creates types page and adds css

---
+++ src/components/TypesPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./TypesPage.css";
+
+const TypesPage = ({ title }) => {
+  return (
+    <div className="types-page">
+      <div className="title"> {title} </div>
+    </div>
+  );
+};
+
+export default TypesPage;
