5470a250c2dd1d877545bd7e74c5e6a1c3ff6d7a
hector <hectornuno1994@gmail.com>
2022-07-15 09:45:50 -0700

    creates navbar and adds css

---
+++ src/components/TopNavbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./TopNavbar.css";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-logo">
+          Type Checkr <i className="fas fa-circle-check" />
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
