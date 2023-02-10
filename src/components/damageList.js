3e6224c842e0a1fd432a0a5647dfef8f164b09c8
hector <hectornuno1994@gmail.com>
2023-02-10 15:15:22 -0800

    creates component for list of types in damageList section

---
+++ src/components/damageList.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./damageList.css";
+import { Types } from "./TypesData";
+
+function DamageList({ title, damage }) {
+  function setBackgroundColor(type) {
+    let color = "";
+    if (Types.some((item) => item.title === type)) {
+      for (let i = 0; i < Types.length; i++) {
+        if (Types[i].title === type) {
+          color = color + Types[i].color;
+        }
+      }
+    }
+
+    return (
+      <div className="type-item" style={{ backgroundColor: `${color}` }}>
+        {type}
+      </div>
+    );
+  }
+
+  return (
+    <div className="wrapper">
+      <h5 className="card-title"> {title} </h5>
+      <ul className="damage-list">
+        {damage?.map((type, index) => {
+          return (
+            <>
+              <li className="damage-item" key={index}>
+                {setBackgroundColor(type.name)}
+              </li>
+            </>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default DamageList;
