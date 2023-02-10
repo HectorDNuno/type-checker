bb92e5e3fc8ce8cf474e5b09f7ed84472d73aa7a
hector <hectornuno1994@gmail.com>
2023-02-10 15:19:35 -0800

    creates a seperate component for listing moves

---
+++ src/components/movesList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./movesList.css";
+
+function MovesList({ title, moves, damageType }) {
+  return (
+    <div className="all-moves">
+      <h4 className={`section-title ${damageType} `}> {title} </h4>
+      <ul className="moves-list">
+        {moves?.map((move, index) => {
+          return (
+            <>
+              <li className="moves" key={index}>
+                {move.damageClass === damageType && <p className="move"> {move.name} </p>}
+              </li>
+            </>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+export default MovesList;
