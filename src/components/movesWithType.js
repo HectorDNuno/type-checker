238e144093b9fc34dbc8c1f3ac802c6e6265bab0
hector <hectornuno1994@gmail.com>
2023-02-11 14:33:57 -0800

    renames file for better undesrstanding of its function

---
+++ src/components/movesWithType.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./movesWithType.css";
+import MovesList from "./movesList";
+
+function movesWithTypeList({ allMoves }) {
+  return (
+    <div className="moves-container">
+      <h2 className="list-title">Moves with type {allMoves.length}</h2>
+      <div className="moves-list-container">
+        <MovesList title={"Physical"} moves={allMoves} damageType={"physical"} />
+        <MovesList title={"Special"} moves={allMoves} damageType={"special"} />
+        <MovesList title={"Status"} moves={allMoves} damageType={"status"} />
+      </div>
+    </div>
+  );
+}
+
+export default movesWithTypeList;
