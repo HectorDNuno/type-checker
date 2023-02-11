a18ca67b2faf9630a4cd72576ef56906da4f4ec7
hector <hectornuno1994@gmail.com>
2023-02-11 14:44:45 -0800

    renamed SidebarData to TypesData

---
+++ src/components/TypesData.js
@@ -0,0 +1,92 @@
+export const Types = [
+  {
+    title: "bug",
+    cName: "sidebar-options",
+    color: "#83c400",
+  },
+  {
+    title: "dark",
+    cName: "sidebar-options",
+    color: "#5a5366",
+  },
+  {
+    title: "dragon",
+    cName: "sidebar-options",
+    color: "#0070ca",
+  },
+  {
+    title: "electric",
+    cName: "sidebar-options",
+    color: "#fbd200",
+  },
+  {
+    title: "fairy",
+    cName: "sidebar-options",
+    color: "#fb8aec",
+  },
+  {
+    title: "fighting",
+    cName: "sidebar-options",
+    color: "#e12c6a",
+  },
+  {
+    title: "fire",
+    cName: "sidebar-options",
+    color: "#ff983e",
+  },
+  {
+    title: "flying",
+    cName: "sidebar-options",
+    color: "#8aace3",
+  },
+  {
+    title: "ghost",
+    cName: "sidebar-options",
+    color: "#4a6ab3",
+  },
+  {
+    title: "grass",
+    cName: "sidebar-options",
+    color: "#33c049",
+  },
+  {
+    title: "ground",
+    cName: "sidebar-options",
+    color: "#e97233",
+  },
+  {
+    title: "ice",
+    cName: "sidebar-options",
+    color: "#4ad2c1",
+  },
+  {
+    title: "normal",
+    cName: "sidebar-options",
+    color: "#929ba4",
+  },
+  {
+    title: "poison",
+    cName: "sidebar-options",
+    color: "#b767ce",
+  },
+  {
+    title: "psychic",
+    cName: "sidebar-options",
+    color: "#ff6676",
+  },
+  {
+    title: "rock",
+    cName: "sidebar-options",
+    color: "#c9b787",
+  },
+  {
+    title: "steel",
+    cName: "sidebar-options",
+    color: "#5a8fa3",
+  },
+  {
+    title: "water",
+    cName: "sidebar-options",
+    color: "#3293dd",
+  },
+];
