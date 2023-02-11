dd8f1db1366757410a27f3fd2f49f8d2e3d628cb
hector <hectornuno1994@gmail.com>
2023-02-11 14:50:58 -0800

    creates typesPageHeader

---
+++ src/components/TypesPageHeader.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./TypesPageHeader.css";
+
+function TypesPageHeader({ type }) {
+  return (
+    <header className="types-page-header">
+      <div className="title-container" style={{ backgroundColor: `${type.color}` }}>
+        <h1 className="type-title">{type.title}</h1>
+      </div>
+    </header>
+  );
+}
+
+export default TypesPageHeader;
