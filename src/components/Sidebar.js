e55a38794f2d67cd2e5ba0a28eb8ee3efeaa18a6
hector <hectornuno1994@gmail.com>
2023-02-11 14:39:49 -0800

    renames props to types, renames useState variables to selectedType, wraps map in a div, returns div instead of ul, removes image tag

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,36 +2,32 @@ import React, { useState } from "react";
 import TypesPage from "./TypesPage";
 import "./Sidebar.css";
 
-const Sidebar = ({ options }) => {
-  const [isClicked, setIsClicked] = useState({ title: "", color: "", imageUrl: "" });
+const Sidebar = ({ types }) => {
+  const [selectedType, setSelectedType] = useState({ title: "", color: "" });
 
   return (
     <>
       <nav className="sidebar">
-        <input className="search-bar" type="search" placeholder="search types..." />
-        <ul>
-          {options.map((type, index) => {
+        <div className="scroll">
+          {types.map((type, index) => {
             return (
-              <>
-                <li className={type.cName} key={index}>
-                  <div
-                    className="individual-options"
-                    onClick={() => {
-                      setIsClicked({ title: `${type.title}`, color: `${type.color}`, imageUrl: `${type.imageUrl}` });
-                    }}
-                    style={{ backgroundColor: `#${type.color}` }}
-                  >
-                    <img className="types-image" src={`${type.imageUrl}`} alt={`${type.title} icon`} />
-                    <span className="dropdown-title"> {type.title} </span>
-                  </div>
-                </li>
-              </>
+              <div className={type.cName} key={index}>
+                <div
+                  className="name-section"
+                  onClick={() => {
+                    setSelectedType({ title: `${type.title}`, color: `${type.color}` });
+                  }}
+                  style={{ backgroundColor: `${type.color}` }}
+                >
+                  <span className="type-name"> {type.title} </span>
+                </div>
+              </div>
             );
           })}
-        </ul>
+        </div>
       </nav>
 
-      {isClicked && <TypesPage type={isClicked} />}
+      {selectedType && <TypesPage type={selectedType} />}
     </>
   );
 };
