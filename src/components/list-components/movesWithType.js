96dafa9c05d44b10963be7d599540f6e4c7c2586
hector <hectornuno1994@gmail.com>
2023-03-21 19:57:30 -0700

    recieves isLoading and type props, conditionally renders number of moves, adds conditions for some for types that don't have moves that include the type name

--- src/components/list-components/movesWithType.js
+++ src/components/list-components/movesWithType.js
@@ -3,10 +3,28 @@ import React from "react";
 import "./movesWithType.css";
 import MovesList from "./movesList";
 
-function movesWithTypeList({ allMoves }) {
+function movesWithTypeList({ allMoves, isLoading, type }) {
   return (
     <div className="moves-container">
-      <h2 className="list-title">Moves with type {allMoves.length}</h2>
+      <div className="list-title">
+        <h2>Moves with type</h2>
+
+        {!isLoading &&
+        allMoves.some(
+          (move) =>
+            move.name.includes(`${type}`) ||
+            move.name.includes("vacuum-wave") ||
+            move.name.includes("wing-attack") ||
+            move.name.includes("trick-or-treat") ||
+            move.name.includes("scorching-sands") ||
+            move.name.includes("yawn")
+        ) ? (
+          <h2 className="number">{allMoves.length}</h2>
+        ) : (
+          <h2 className="number skeleton"></h2>
+        )}
+      </div>
+
       <div className="moves-list-container">
         <MovesList title={"Physical"} moves={allMoves} damageType={"physical"} />
         <MovesList title={"Special"} moves={allMoves} damageType={"special"} />
