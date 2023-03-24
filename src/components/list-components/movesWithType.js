4442c0c1f0289e140a302777b296d83111791e9a
hector <hectornuno1994@gmail.com>
2023-03-23 22:35:31 -0700

    removes title because it was redundant, renames skeleton class to number-skeleton, adds colon in h2

--- src/components/list-components/movesWithType.js
+++ src/components/list-components/movesWithType.js
@@ -7,11 +7,10 @@ function movesWithTypeList({ allMoves, isLoading, type }) {
   const physicalMoves = allMoves.filter((move) => move.damageClass === "physical");
   const specialMoves = allMoves.filter((move) => move.damageClass === "special");
   const statusMoves = allMoves.filter((move) => move.damageClass === "status");
-
   return (
     <div className="moves-container">
       <div className="moves-section-title">
-        <h2>Moves with type</h2>
+        <h2>Moves with type:</h2>
 
         {!isLoading &&
         allMoves.some(
@@ -25,14 +24,14 @@ function movesWithTypeList({ allMoves, isLoading, type }) {
         ) ? (
           <h2 className="number">{allMoves.length}</h2>
         ) : (
-          <h2 className="number skeleton"></h2>
+          <h2 className="number number-skeleton"></h2>
         )}
       </div>
 
       <div className="moves-list-container">
-        <MovesList title={"Physical"} moves={physicalMoves} damageType={"physical"} />
-        <MovesList title={"Special"} moves={specialMoves} damageType={"special"} />
-        <MovesList title={"Status"} moves={statusMoves} damageType={"status"} />
+        <MovesList moves={physicalMoves} damageType={"physical"} isLoading={isLoading} />
+        <MovesList moves={specialMoves} damageType={"special"} isLoading={isLoading} />
+        <MovesList moves={statusMoves} damageType={"status"} isLoading={isLoading} />
       </div>
     </div>
   );
