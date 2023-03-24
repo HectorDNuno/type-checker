f6aee4bab969616f590bafd04e0dde307c3ac553
hector <hectornuno1994@gmail.com>
2023-03-23 22:39:43 -0700

    adds moves count

--- src/components/list-components/movesList.js
+++ src/components/list-components/movesList.js
@@ -6,6 +6,7 @@ function MovesList({ title, moves, damageType }) {
   return (
     <div className="all-moves">
       <h4 className={`section-title ${damageType} `}> {title} </h4>
+      <div className="moves-count">{moves.length} moves </div>
       <ul className="moves-list">
         {moves
           ?.sort((a, b) => (a.name > b.name ? 1 : b.name > a.name ? -1 : 0))
