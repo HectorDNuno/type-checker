5c8c8ea7b3c145b9d9c4161e66e9795a8f90dc28
hector <hectornuno1994@gmail.com>
2023-04-08 21:35:53 -0700

    uses context api and updates variable names

--- src/components/types-page-components/typesPageHeader.js
+++ src/components/types-page-components/typesPageHeader.js
@@ -1,12 +1,15 @@
 /* eslint-disable */
-import React from "react";
+import React, { useContext } from "react";
+import { selectedTypeContext } from "../../selectedTypeContext";
 import "./typesPageHeader.css";
 
-function TypesPageHeader({ type }) {
+function TypesPageHeader() {
+  const { selectedType } = useContext(selectedTypeContext);
+
   return (
     <header className="types-page-header">
-      <div className="title-container" style={{ backgroundColor: `${type.color}` }}>
-        <h1 className="type-title">{type.title}</h1>
+      <div className="title-container" style={{ backgroundColor: `${selectedType.color}` }}>
+        <h1 className="type-title">{selectedType.title}</h1>
       </div>
     </header>
   );
