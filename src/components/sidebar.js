e23e94ce6ff3296a69918c8f83cfdb1e0a1e9db9
Hector Nuno <hectornuno1994@gmail.com>
2023-09-11 23:20:06 -0700

    Enhance Sidebar Component
    - Refactor code to directly use imported Types data
    - Implement text color change based on selection
    - Improve code readability and maintainability

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,53 +1,43 @@
 /* eslint-disable */
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { selectedTypeContext } from "../selectedTypeContext";
 import { Types } from "./typesData";
 import "./sidebar.css";
 
 const Sidebar = () => {
   const { setSelectedType } = useContext(selectedTypeContext);
-  const types = Types;
 
-  const changeSelectedTypeColor = (e, color) => {
-    const allSideBarOptions = document.querySelectorAll(".name-section");
-    allSideBarOptions.forEach((option) => {
-      option.style.backgroundColor = "";
-      option.style.color = "#000000";
-    });
+  // Initialize state for the index of the currently selected type
+  const [selectedTypeIndex, setSelectedTypeIndex] = useState(null);
 
-    const selected = e.target;
-    selected.style.backgroundColor = color;
-    selected.style.color = "#FFFFFF";
-  };
+  const handleTypeClick = (type, index) => {
+    setSelectedType({ title: type.title, color: type.color });
 
-  const resetListPosition = () => {
-    const list = document.getElementById("list");
-    list.scrollTop = 0;
+    // Update the selected type index
+    setSelectedTypeIndex(index);
   };
 
   return (
-    <>
-      <nav className="sidebar">
-        <div className="scroll">
-          {types.map((type, index) => {
-            return (
-              <div className="sidebar-options" key={index}>
-                <div
-                  className="name-section"
-                  onClick={(e) => {
-                    setSelectedType({ title: `${type.title}`, color: `${type.color}` });
-                    changeSelectedTypeColor(e, type.color);
-                    resetListPosition();
-                  }}
-                >
-                  {type.title}
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      </nav>
-    </>
+    <nav className="sidebar">
+      <div className="scroll">
+        {Types.map((type, index) => (
+          <div className="sidebar-options" key={index}>
+            <div
+              className="name-section"
+              style={{
+                backgroundColor: index === selectedTypeIndex ? type.color : "#D3D3D3",
+                color: index === selectedTypeIndex ? "#FFFFFF" : "#000000",
+              }}
+              onClick={() => {
+                handleTypeClick(type, index);
+              }}
+            >
+              {type.title}
+            </div>
+          </div>
+        ))}
+      </div>
+    </nav>
   );
 };
 
