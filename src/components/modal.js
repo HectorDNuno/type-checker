7be03fb46bccd9d7a046b9baa3ec67954957fb6b
Hector Nuno <hectornuno1994@gmail.com>
2024-04-15 13:14:47 -0700

    creates some basic markup for modal

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,7 +1,19 @@
 import React from "react";
+import "./modal.css";
 
-const Modal = () => {
-  return <div>Modal</div>;
+const Modal = ({ trigger, setTrigger, content }) => {
+  return trigger ? (
+    <div className="modal">
+      <div className="modal-inner">
+        <p> {content} </p>
+        <button onClick={() => setTrigger({ isOpen: false })} className="close-btn">
+          <i className="fa-regular fa-circle-xmark fa-lg"></i>
+        </button>
+      </div>
+    </div>
+  ) : (
+    ""
+  );
 };
 
 export default Modal;
