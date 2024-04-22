7b68920149b1b76d6855aa6deced4ded060afb7d
Hector Nuno <hectornuno1994@gmail.com>
2024-04-21 20:39:54 -0700

    begins processs of splitting up modal content into components and makes adjustments in css files

---
+++ src/components/infoTab.js
@@ -0,0 +1,144 @@
+import React, { useMemo } from "react";
+import "./infoTab.css";
+import { Types } from "../typesData";
+import Barchart from "./barchart";
+
+const InfoTab = ({ pokemonInfo, shinySprite }) => {
+  const setColor = (typeFromList) => {
+    const typeColor = Types.find((type) => type.title === typeFromList)?.color;
+    return typeColor;
+  };
+
+  const findRecentSpeciesData = (entries) => {
+    if (!entries || entries.length === 0) return null;
+
+    const filteredByLanguage = entries.filter((entry) => entry.language.name === "en");
+    const versionNumbers = filteredByLanguage.map((entry) => parseInt(entry.version.url.split("/").slice(-2, -1)[0]));
+    const maxVersionNumber = Math.max(...versionNumbers);
+
+    return filteredByLanguage.find(
+      (entry) => parseInt(entry.version.url.split("/").slice(-2, -1)[0]) === maxVersionNumber
+    );
+  };
+
+  const reorganizeStats = (stats) => {
+    return (
+      stats?.map(({ base_stat, stat }) => ({
+        name: stat.name,
+        base_stat,
+      })) ?? []
+    );
+  };
+
+  const getFormattedGeneration = (generationString) => {
+    const generationMap = {
+      "generation-i": "Generation 1",
+      "generation-ii": "Generation 2",
+      "generation-iii": "Generation 3",
+      "generation-iv": "Generation 4",
+      "generation-v": "Generation 5",
+      "generation-vi": "Generation 6",
+      "generation-vii": "Generation 7",
+      "generation-viii": "Generation 8",
+      "generation-viiii": "Generation 9",
+      "generation-x": "Generation 10",
+    };
+
+    return generationMap[generationString] || generationString;
+  };
+
+  const classification = useMemo(
+    () => pokemonInfo.speciesData.genera?.find((entry) => entry.language.name === "en"),
+    [pokemonInfo.speciesData.genera]
+  );
+  const entry = findRecentSpeciesData(pokemonInfo.speciesData?.flavor_text_entries);
+  const stats = reorganizeStats(pokemonInfo.pokedexData?.stats);
+  const formattedGeneration = getFormattedGeneration(pokemonInfo.speciesData.generation?.name);
+
+  const imageUrl = shinySprite
+    ? pokemonInfo.pokedexData.sprites?.other.home.front_shiny
+    : pokemonInfo.pokedexData.sprites?.other.home.front_default;
+
+  return (
+    <div className="info-tab">
+      {pokemonInfo.pokedexData.sprites ? (
+        <img src={imageUrl} alt={`${pokemonInfo.pokedexData.name} sprite`} />
+      ) : (
+        <img alt="" />
+      )}
+
+      <div className="info">
+        <div> #{pokemonInfo.pokedexData.id} </div>
+        <h4> {pokemonInfo.pokedexData.name} </h4>
+        <div>{pokemonInfo.speciesData.genera && pokemonInfo.speciesData.genera.length > 0 && classification.genus}</div>
+
+        {pokemonInfo.pokedexData.types && (
+          <div className="types">
+            <div className="type" style={{ backgroundColor: setColor(pokemonInfo.pokedexData.types[0].type.name) }}>
+              {pokemonInfo.pokedexData.types[0].type.name}{" "}
+            </div>
+            {pokemonInfo.pokedexData.types[1] && (
+              <div className="type" style={{ backgroundColor: setColor(pokemonInfo.pokedexData.types[1].type.name) }}>
+                {pokemonInfo.pokedexData.types[1].type.name}{" "}
+              </div>
+            )}
+          </div>
+        )}
+        <div className="pokedex-entry">{pokemonInfo.speciesData.flavor_text_entries && entry.flavor_text}</div>
+      </div>
+
+      <div className="stats">
+        <Barchart stats={stats} />
+      </div>
+
+      <div className="abilities">
+        <h5>Abilities</h5>
+        {pokemonInfo.pokedexData.types && (
+          <>
+            <div className="ability-one">
+              <div className="ability-name" style={{ color: setColor(pokemonInfo.pokedexData.types[0].type.name) }}>
+                {pokemonInfo.abilityEntries.abilityOne.name}
+              </div>
+              <div> {pokemonInfo.abilityEntries.abilityOne.flavorText} </div>
+            </div>
+            {pokemonInfo.abilityEntries.abilityTwo.name &&
+              pokemonInfo.abilityEntries.abilityTwo.name !== pokemonInfo.abilityEntries.abilityOne.name && (
+                <div>
+                  <div className="ability-name" style={{ color: setColor(pokemonInfo.pokedexData.types[0].type.name) }}>
+                    {pokemonInfo.abilityEntries.abilityTwo.name} <span> (hidden ability) </span>
+                  </div>
+                  <div> {pokemonInfo.abilityEntries.abilityTwo.flavorText} </div>
+                </div>
+              )}
+          </>
+        )}
+      </div>
+
+      <div className="characteristics">
+        <h5>Characteristics</h5>
+        {pokemonInfo.pokedexData.weight && pokemonInfo.speciesData.base_happiness && (
+          <>
+            <div style={{ color: setColor(pokemonInfo.pokedexData.types[0].type.name) }}>Weight</div>
+            <div> {(pokemonInfo.pokedexData.weight / 4.536).toFixed(1)} lbs</div>
+            <div style={{ color: setColor(pokemonInfo.pokedexData.types[0].type.name) }}>Height</div>
+            <div> {(pokemonInfo.pokedexData.height / 3.048).toFixed(1)} lbs</div>
+            <div style={{ color: setColor(pokemonInfo.pokedexData.types[0].type.name) }}>Base Experience</div>
+            <div> {pokemonInfo.pokedexData.base_experience}</div>
+            <div style={{ color: setColor(pokemonInfo.pokedexData.types[0].type.name) }}>Base Happiness</div>
+            <div> {pokemonInfo.speciesData.base_happiness}</div>
+            <div style={{ color: setColor(pokemonInfo.pokedexData.types[0].type.name) }}>Catch Rate</div>
+            <div> {pokemonInfo.speciesData.capture_rate}</div>
+            <div style={{ color: setColor(pokemonInfo.pokedexData.types[0].type.name) }}>Growth Rate</div>
+            <div> {pokemonInfo.speciesData.growth_rate.name}</div>
+            <div style={{ color: setColor(pokemonInfo.pokedexData.types[0].type.name) }}>Hatch Time</div>
+            <div> {pokemonInfo.speciesData.hatch_counter}</div>
+            <div style={{ color: setColor(pokemonInfo.pokedexData.types[0].type.name) }}>First Appeared</div>
+            <div> {formattedGeneration}</div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default InfoTab;
--- src/components/modal.js
+++ src/components/modal.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useMemo, useRef, useState } from "react";
+import React, { useEffect, useRef, useState, useContext } from "react";
 import "./modal.css";
 import axios from "axios";
+import InfoTab from "./infoTab";
+import { SelectedTypeContext } from "../Contexts";
 import { Types } from "../typesData";
-import Barchart from "./barchart";
 
 const Modal = ({ closeModal, content, shinySprite }) => {
   const [pokemonInfo, setPokemonInfo] = useState({
@@ -14,7 +15,10 @@ const Modal = ({ closeModal, content, shinySprite }) => {
     },
   });
 
+  const [activeTab, setActiveTab] = useState(1);
+
   const modalRef = useRef(null);
+  const { selectedType } = useContext(SelectedTypeContext);
 
   useEffect(() => {
     let isMounted = true;
@@ -146,23 +150,6 @@ const Modal = ({ closeModal, content, shinySprite }) => {
     };
   }, [closeModal]);
 
-  const setBackgroundColor = (typeFromList) => {
-    const typeColor = Types.find((type) => type.title === typeFromList)?.color;
-    return typeColor;
-  };
-
-  const findRecentSpeciesData = (entries) => {
-    if (!entries || entries.length === 0) return null;
-
-    const filteredByLanguage = entries.filter((entry) => entry.language.name === "en");
-    const versionNumbers = filteredByLanguage.map((entry) => parseInt(entry.version.url.split("/").slice(-2, -1)[0]));
-    const maxVersionNumber = Math.max(...versionNumbers);
-
-    return filteredByLanguage.find(
-      (entry) => parseInt(entry.version.url.split("/").slice(-2, -1)[0]) === maxVersionNumber
-    );
-  };
-
   const findHighestAbilityEntry = (entries) => {
     if (!entries || entries.length === 0) return null;
 
@@ -177,151 +164,43 @@ const Modal = ({ closeModal, content, shinySprite }) => {
     );
   };
 
-  const reorganizeStats = (stats) => {
-    return (
-      stats?.map(({ base_stat, stat }) => ({
-        name: stat.name,
-        base_stat,
-      })) ?? []
-    );
-  };
-
-  const getFormattedGeneration = (generationString) => {
-    const generationMap = {
-      "generation-i": "Generation 1",
-      "generation-ii": "Generation 2",
-      "generation-iii": "Generation 3",
-      "generation-iv": "Generation 4",
-      "generation-v": "Generation 5",
-      "generation-vi": "Generation 6",
-      "generation-vii": "Generation 7",
-      "generation-viii": "Generation 8",
-      "generation-viiii": "Generation 9",
-      "generation-x": "Generation 10",
-    };
-
-    return generationMap[generationString] || generationString;
-  };
-
-  const classification = useMemo(
-    () => pokemonInfo.speciesData.genera?.find((entry) => entry.language.name === "en"),
-    [pokemonInfo.speciesData.genera]
-  );
-  const entry = findRecentSpeciesData(pokemonInfo.speciesData?.flavor_text_entries);
-  const stats = reorganizeStats(pokemonInfo.pokedexData?.stats);
-  const formattedGeneration = getFormattedGeneration(pokemonInfo.speciesData.generation?.name);
-
-  const imageUrl = shinySprite
-    ? pokemonInfo.pokedexData.sprites?.other.home.front_shiny
-    : pokemonInfo.pokedexData.sprites?.other.home.front_default;
-
   return (
     <div className="modal">
       <div ref={modalRef} className="modal-inner">
+        <div className="tab-nav">
+          <button
+            onClick={() => setActiveTab(1)}
+            style={{ backgroundColor: activeTab === 1 ? selectedType.color : "" }}
+          >
+            <i className="fa-solid fa-circle-info"></i> info
+          </button>
+
+          <button
+            onClick={() => setActiveTab(2)}
+            style={{ backgroundColor: activeTab === 2 ? selectedType.color : "" }}
+          >
+            <i className="fa-solid fa-circle-nodes"></i>
+            evolutions
+          </button>
+
+          <button
+            onClick={() => setActiveTab(3)}
+            style={{ backgroundColor: activeTab === 3 ? selectedType.color : "" }}
+          >
+            <i className="fa-solid fa-bolt"></i>
+            moves
+          </button>
+        </div>
+
         <button onClick={closeModal} className="close-btn">
           <i className="fa-regular fa-circle-xmark fa-lg"></i>
         </button>
 
-        {pokemonInfo.pokedexData.sprites ? (
-          <img src={imageUrl} alt={`${pokemonInfo.pokedexData.name} sprite`} />
-        ) : (
-          <img alt="" />
-        )}
+        <div className="modal-content">
+          {activeTab === 1 && <InfoTab pokemonInfo={pokemonInfo} shinySprite={shinySprite} />}
 
-        <div className="info">
-          <div> #{pokemonInfo.pokedexData.id} </div>
-          <h4> {pokemonInfo.pokedexData.name} </h4>
-          <div>
-            {" "}
-            {pokemonInfo.speciesData.genera && pokemonInfo.speciesData.genera.length > 0 && classification.genus}{" "}
-          </div>
-
-          {pokemonInfo.pokedexData.types && (
-            <div className="types">
-              <div
-                className="type"
-                style={{ backgroundColor: setBackgroundColor(pokemonInfo.pokedexData.types[0].type.name) }}
-              >
-                {pokemonInfo.pokedexData.types[0].type.name}{" "}
-              </div>
-              {pokemonInfo.pokedexData.types[1] && (
-                <div
-                  className="type"
-                  style={{ backgroundColor: setBackgroundColor(pokemonInfo.pokedexData.types[1].type.name) }}
-                >
-                  {pokemonInfo.pokedexData.types[1].type.name}{" "}
-                </div>
-              )}
-            </div>
-          )}
-
-          <div className="pokedex-entry">{pokemonInfo.speciesData.flavor_text_entries && entry.flavor_text}</div>
-        </div>
-
-        <div className="stats">
-          <Barchart stats={stats} />
-        </div>
-
-        <div className="abilities">
-          <h5>Abilities</h5>
-
-          {pokemonInfo.pokedexData.types && (
-            <>
-              <div className="ability-one">
-                <div
-                  className="ability-name"
-                  style={{ color: setBackgroundColor(pokemonInfo.pokedexData.types[0].type.name) }}
-                >
-                  {pokemonInfo.abilityEntries.abilityOne.name}
-                </div>
-                <div> {pokemonInfo.abilityEntries.abilityOne.flavorText} </div>
-              </div>
-
-              {pokemonInfo.abilityEntries.abilityTwo.name &&
-                pokemonInfo.abilityEntries.abilityTwo.name !== pokemonInfo.abilityEntries.abilityOne.name && (
-                  <div>
-                    <div
-                      className="ability-name"
-                      style={{ color: setBackgroundColor(pokemonInfo.pokedexData.types[0].type.name) }}
-                    >
-                      {pokemonInfo.abilityEntries.abilityTwo.name} <span> (hidden ability) </span>
-                    </div>
-                    <div> {pokemonInfo.abilityEntries.abilityTwo.flavorText} </div>
-                  </div>
-                )}
-            </>
-          )}
-        </div>
-
-        <div className="characteristics">
-          <h5>Characteristics</h5>
-
-          {pokemonInfo.pokedexData.weight && pokemonInfo.speciesData.base_happiness && (
-            <>
-              <div style={{ color: setBackgroundColor(pokemonInfo.pokedexData.types[0].type.name) }}>Weight</div>
-              <div> {(pokemonInfo.pokedexData.weight / 4.536).toFixed(1)} lbs</div>
-              <div style={{ color: setBackgroundColor(pokemonInfo.pokedexData.types[0].type.name) }}>Height</div>
-              <div> {(pokemonInfo.pokedexData.height / 3.048).toFixed(1)} lbs</div>
-              <div style={{ color: setBackgroundColor(pokemonInfo.pokedexData.types[0].type.name) }}>
-                Base Experience
-              </div>
-              <div> {pokemonInfo.pokedexData.base_experience}</div>
-              <div style={{ color: setBackgroundColor(pokemonInfo.pokedexData.types[0].type.name) }}>
-                Base Happiness
-              </div>
-              <div> {pokemonInfo.speciesData.base_happiness}</div>
-              <div style={{ color: setBackgroundColor(pokemonInfo.pokedexData.types[0].type.name) }}>Catch Rate</div>
-              <div> {pokemonInfo.speciesData.capture_rate}</div>
-              <div style={{ color: setBackgroundColor(pokemonInfo.pokedexData.types[0].type.name) }}>Growth Rate</div>
-              <div> {pokemonInfo.speciesData.growth_rate.name}</div>
-              <div style={{ color: setBackgroundColor(pokemonInfo.pokedexData.types[0].type.name) }}>Hatch Time</div>
-              <div> {pokemonInfo.speciesData.hatch_counter}</div>
-              <div style={{ color: setBackgroundColor(pokemonInfo.pokedexData.types[0].type.name) }}>
-                First Appeared
-              </div>
-              <div> {formattedGeneration}</div>
-            </>
-          )}
+          {activeTab === 2 && <h1>Evolutions</h1>}
+          {activeTab === 3 && <h1>Moves</h1>}
         </div>
       </div>
     </div>
